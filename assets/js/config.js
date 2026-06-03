65313e179d08b3c6c83bac1cc6a033d91cc123b1
David Ellams <davidellams@hotmail.com>
2026-06-03 03:32:22 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,3 @@
-window.apiUrl = 'https://oasisapionode-hseudrexdvbhenhv.canadacentral-01.azurewebsites.net';
+//window.apiUrl = 'https://oasisapionode-hseudrexdvbhenhv.canadacentral-01.azurewebsites.net';
+window.apiUrl = 'https://api.oasisweb4.one';
 window.API_BASE = window.apiUrl;
